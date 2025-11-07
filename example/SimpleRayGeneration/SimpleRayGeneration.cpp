a96ed502968f4312268c529473cafe58ba4f9a25
Connor <wxj995@gmail.com>
2025-11-07 09:57:11 -0800

    add simpleraygeneration

---
+++ example/SimpleRayGeneration/SimpleRayGeneration.cpp
@@ -0,0 +1,24 @@
+#include "IGame.h"
+#include "TypeVertex.h"
+
+namespace LuminError{
+    class Game : public IGame {
+        std::vector<Vertex3D> vertices3D = {
+            { { -0.5f, -0.5f, 0.0f },{ 1.0f, 0.0f, 0.0f },{ 1.0f, 0.0f } ,{ 0.0f, 0.0f, 1.0f }},
+            { { 0.5f, -0.5f, 0.0f },{ 0.0f, 1.0f, 0.0f },{ 0.0f, 0.0f } ,{ 0.0f, 0.0f, 1.0f }},
+            { { 0.5f, 0.5f, 0.0f },{ 0.0f, 0.0f, 1.0f },{ 0.0f, 1.0f } ,{ 0.0f, 0.0f, 1.0f }},
+            { { -0.5f, 0.5f, 0.0f },{ 1.0f, 1.0f, 1.0f },{ 1.0f, 1.0f } ,{ 0.0f, 0.0f, 1.0f }}
+        };
+        std::vector<uint32_t> indices3D = { 0, 1, 2, 2, 3, 0};
+        
+        void Initialize() override {
+            GameEngine->CreateCustomModel3D(vertices3D, indices3D);
+        }
+
+        void Record() override{
+            GameEngine->DrawObjects();
+            GameEngine->DrawTexts();
+        }
+    };
+}
+#include "Launcher.hpp"
