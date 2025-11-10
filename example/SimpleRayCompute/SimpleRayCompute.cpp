f77bfb27f1ef67e5b81ca99189def3f028519fec
Connor <wxj995@gmail.com>
2025-11-09 21:51:24 -0800

    add simpleraycompute(empty)

---
+++ example/SimpleRayCompute/SimpleRayCompute.cpp
@@ -0,0 +1,26 @@
+/* ***********
+ * Implement Ray Tracking using compute pipeline
+ * Use SSBO(Shader Storage Buffer Object, or compute storage uniform) as input (and internal output)
+ * Use swapchain image as final output
+ * *********** */
+#include "IGame.h"
+#include "Enum.h"
+#include "Config.h"
+namespace LuminError{
+    class Game : public IGame {
+        void PreInitialize() override {
+            GameEngine->SetSwapchainImageSize(MAX_FRAMES_IN_FLIGHT);
+            GameEngine->EnableComputeSwapChainImage(true);
+	    }
+
+        void Initialize() override {
+            GameEngine->SetRenderMode(RenderModes::COMPUTE_SWAPCHAIN);
+        }
+
+        void PostInitialize() override{
+            //Note: in this test, commandBuffers are recorded once(because they do not change every frame)
+            GameEngine->CreateComputeCommandBuffers_DispatchForSwapchainImage(200, 300, 1);
+        }
+    };
+}
+#include "Launcher.hpp"
